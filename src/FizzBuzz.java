d5d5ae4e8b93b5e96c37d75b5f148d3d76bc6c0d
izabellemarianne <izabellemarianne08@gmail.com>
2024-09-09 12:24:18 -0400

    random chnages

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
 
         for (int i = 1; i < 100; i++) {
              // Find out which numbers divide i.
@@ -44,7 +44,7 @@ class FizzBuzz {
         return j;
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
